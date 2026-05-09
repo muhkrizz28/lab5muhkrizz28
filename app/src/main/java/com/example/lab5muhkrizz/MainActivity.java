8152a6c12a4695c734b1e4f4313900bc70272c42
Muhkrizz <2023267074@student.uitm.edu.my>
2026-05-09 16:35:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab5muhkrizz/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lab5muhkrizz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openRegister(View v){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+
+    public void openLogin(View v){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/lab5muhkrizz/ProfileActivity.java
@@ -0,0 +1,45 @@
+package com.example.lab5muhkrizz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    TextView profileText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        auth = FirebaseAuth.getInstance();
+
+        profileText = findViewById(R.id.textView);
+
+        user = auth.getCurrentUser();
+
+        if(user != null){
+            profileText.setText(user.getEmail());
+        }
+    }
+
+    public void signout(View v){
+
+        auth.signOut();
+
+        finish();
+
+        Intent i = new Intent(this, MainActivity.class);
+
+        startActivity(i);
+    }
+}
